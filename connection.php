53aff3bf3ecc462840a47b0f3484fc42b883fe15
Aniket <aniketmetkari14@gmail.com>
2022-03-18 14:37:40 +0530

    Connection file

---
+++ connection.php
@@ -0,0 +1,16 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = " ";
+$dbname = "registerdetail";
+
+$conn = mysqli_connect("localhost","root","","registerdetail");
+if($conn)
+{
+	// echo "Ok";
+}
+else
+{
+	// echo "Not ok";
+}
+?>
